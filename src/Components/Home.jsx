67b9baefebc11d6200d5ca245bc387baeedced9d
Gioele Bucci <gioelebucci@gmail.com>
2023-11-11 17:56:55 +0000

    render home for mobile

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,7 +12,7 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
@@ -22,6 +22,20 @@ import PropTypes from "prop-types";
 import image from "../images/woman-with-tablet.jpg";
 
 const Home = ({ name, title }) => {
+  const isMobile = window.matchMedia("(max-width: 767px)").matches;
+
+  if (isMobile) {
+    return (
+      <section id="home" className="min-height">
+        <img className="background" src={image} alt="" style={{ maxWidth: "100%", maxHeight: "100%" }} />
+        <div style={{ position: "absolute", top: "2rem", left: "1rem", width: "15rem" }}>
+          <br />
+          <h2 style={{ fontWeight: "bold" }}>{name}</h2>
+          <h3 style={{ fontWeight: "italic" }}>{title}</h3>
+        </div>
+      </section>
+    );
+  }
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
