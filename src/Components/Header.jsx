8744dd77300b2953e70122b870a69c8cc2a5a91d
Gioele Bucci <gioelebucci@gmail.com>
2024-07-31 12:24:02 +0000

    add language swap

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+
+import enFlag from "../images/flags/en.svg";
+import itFlag from "../images/flags/it.svg";
 
 const Header = () => {
+  const [t, i18n] = useTranslation("global");
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+
+  const handleFlagClick = () => {
+    changeLanguage(i18n.language === "en" ? "it" : "en");
+  };
+
   return (
     <div
       style={{
@@ -23,6 +37,25 @@ const Header = () => {
       <a href="#about">About</a>
       <a href="#portfolio">Portfolio</a>
       <a href="#footer">Contact</a>
+      <button
+        style={{
+          background: "none",
+          border: "none",
+          cursor: "pointer",
+          padding: 0,
+        }}
+        onClick={handleFlagClick}
+      >
+        <img
+          src={enFlag}
+          alt="Flag"
+          style={{
+            width: "auto",
+            height: "24px",
+            alignSelf: "right",
+          }}
+        />
+      </button>
     </div>
   );
 };
